2a4c0fa59911fec0efcc05797af5400770fca6ec
Charlie Moonen <shmincky1@gmail.com>
2016-03-06 16:28:44 -0600

    kgh

---
+++ code/modules/jt/art/tiles/truff.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="truff" tilewidth="64" tileheight="32" tilecount="4">
+ <tile id="0">
+  <image width="32" height="32" source="tile 1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="tile 2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="32" source="tile 3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="tile 4.png"/>
+ </tile>
+</tileset>
